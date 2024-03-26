7272db9231394ce135ae0beaca447c61ea856464
erixweb <erikmorenodev@gmail.com>
2024-03-26 19:38:36 +0100

    Add form

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,5 +1,11 @@
-export default function AddPage () {
-    return (
-        <form action=""></form>
-    )
+"use client"
+
+export default function AddPage() {
+	return (
+		<form action="/api/create" method="POST">
+			<input type="text" name="href" />
+			<input type="text" name="original" />
+			<button>submit</button>
+		</form>
+	)
 }
